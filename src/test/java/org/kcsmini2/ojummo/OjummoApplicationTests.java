9d6b64492703db5225f542c44a9f7b41c3ac7bce
TaeMin Kim <tk4003950@gmail.com>
2023-07-20 12:02:49 +0900

    🎉	:  스프링부트 프로젝트 시작
    - Spring Initializr 로 스프링부트 프로젝트 시작
    - gitignore.io 로 .gitignore 작성

---
+++ src/test/java/org/kcsmini2/ojummo/OjummoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kcsmini2.ojummo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjummoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
