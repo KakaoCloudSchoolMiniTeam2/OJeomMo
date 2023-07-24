b7e204d83be925ceab968f9189f17c1a11facaca
kanari1569 <kanari1569@gmail.com>
2023-07-24 17:20:38 +0900

    ✨	:  Jwt 발급, 권한 정보 획득, 인증,  Jwt Filter, SecurityConfig 설정

---
+++ src/main/java/org/kcsmini2/ojeommo/config/jwt/JpaUserDetailsService.java
@@ -0,0 +1,26 @@
+package org.kcsmini2.ojeommo.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.kcsmini2.ojeommo.member.entity.Member;
+import org.kcsmini2.ojeommo.member.repository.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findById(username).orElseThrow(
+                () -> new UsernameNotFoundException("Invalid authentication!")
+        );
+
+        return new MemberDetail(member);
+    }
+}
