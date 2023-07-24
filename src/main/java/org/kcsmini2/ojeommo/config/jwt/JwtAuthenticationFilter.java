39263520a6bba6b460472f19170330afff97661a
kanari1569 <kanari1569@gmail.com>
2023-07-24 17:20:38 +0900

    ✨	:  Jwt 발급, 권한 정보 획득, 인증,  Jwt Filter, SecurityConfig 설정

---
+++ src/main/java/org/kcsmini2/ojeommo/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package org.kcsmini2.ojeommo.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    public JwtAuthenticationFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, IOException {
+        String token = jwtProvider.resolveToken(request);
+
+        if (token != null && jwtProvider.validateToken(token)) {
+            // check access token
+            token = token.split(" ")[1].trim();
+            Authentication auth = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
---
+++ src/main/java/org/kcsmini2/ojeommo/config/jwt/JwtProvider.java
@@ -0,0 +1,99 @@
+package org.kcsmini2.ojeommo.config.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.kcsmini2.ojeommo.member.entity.Authority;
+import org.kcsmini2.ojeommo.member.entity.Member;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * 작성자: 김준연
+ *
+ * 설명: jwt 토큰 생성, 검증, 정보 로딩 클래스
+ *
+ * 최종 수정 일자: 2023/07/21
+ */
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret.key}")
+    private String salt;
+
+    private Key secretKey;
+
+    // 만료시간 : 1Hour
+    private final long exp = 1000L * 60 * 60;
+
+    private final JpaUserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // 토큰 생성
+    public String createToken(String id, List<Authority> roles) {
+        Claims claims = Jwts.claims().setSubject(id);
+        claims.put("roles", roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + exp))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 권한정보 획득
+    // Spring Security 인증과정에서 권한확인을 위한 기능
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getAccount(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // 토큰에 담겨있는 유저 account 획득
+    public String getAccount(String token) {
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Authorization Header를 통해 인증을 한다.
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            // Bearer 검증
+            if (!token.substring(0, "BEARER ".length()).equalsIgnoreCase("BEARER ")) {
+                return false;
+            } else {
+                token = token.split(" ")[1].trim();
+            }
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            // 만료되었을 시 false
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
