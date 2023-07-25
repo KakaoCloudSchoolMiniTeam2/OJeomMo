b474887a141587cb2c1c8392cc92bc877540c403
whdals9711 <70644199+whdals9711@users.noreply.github.com>
2023-07-25 14:20:51 +0900

    🔥	: Dummy File 삭제

--- src/main/java/org/kcsmini2/ojeommo/board/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.kcsmini2.ojeommo.board.repository;
-
-import org.kcsmini2.ojeommo.member.data.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,String> {
-}
