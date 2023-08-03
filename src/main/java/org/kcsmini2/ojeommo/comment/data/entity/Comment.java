32d96bb5b0582a0c3ffb07da3c0050581c9e1198
TaeMin Kim <tk4003950@gmail.com>
2023-08-03 17:49:59 +0900

    ✨	: Comment 엔티티 인덱스 추기

--- src/main/java/org/kcsmini2/ojeommo/comment/data/entity/Comment.java
+++ src/main/java/org/kcsmini2/ojeommo/comment/data/entity/Comment.java
@@ -2,12 +2,18 @@ package org.kcsmini2.ojeommo.comment.data.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.kcsmini2.ojeommo.board.data.entity.Board;
+import org.kcsmini2.ojeommo.comment.data.dto.request.CommentUpdateRequestDTO;
 import org.kcsmini2.ojeommo.member.data.entity.Member;
 
 @Entity (name= "comment")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(name = "idx__table", columnList = "board_id")
+})
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,10 +21,12 @@ public class Comment {
 
     @JoinColumn(name = "author_id")
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Member author;
 
     @JoinColumn(name = "board_id")
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Board board;
 
     @Column
@@ -29,5 +37,14 @@ public class Comment {
         this.author = author;
         this.board = board;
         this.content = content;
+    }
+
+    public Comment update(CommentUpdateRequestDTO requestDTO) {
+        this.content = requestDTO.getContent();
+        return this;
+    }
+
+    public boolean isWrongAuthor(Member requester) {
+        return !this.author.equals(requester);
     }
 }
