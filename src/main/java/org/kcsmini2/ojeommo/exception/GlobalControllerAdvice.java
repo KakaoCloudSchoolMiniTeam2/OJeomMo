b8760fa3f4d54b9f5c0a6326de50b934116bbbdc
KJY <rntlls1366@gmail.com>
2023-08-03 20:11:08 +0900

    🎨	: jwt 인증 실패로 인한 AuthenticationException 발생시 /login 으로 리다이렉트 되도록 변경

--- src/main/java/org/kcsmini2/ojeommo/exception/GlobalControllerAdvice.java
+++ src/main/java/org/kcsmini2/ojeommo/exception/GlobalControllerAdvice.java
@@ -1,17 +1,14 @@
 package org.kcsmini2.ojeommo.exception;
 
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import java.io.IOException;
 
@@ -31,7 +28,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(AuthenticationException.class)
     public HttpEntity<String> jwtExHandler() {
-        return new HttpEntity<>("<script>alert('" + "로그인 후 이용해주세요." + "'); history.go(-1);</script>");
+        return new HttpEntity<>("<script>window.location.href = '/login';</script>");
     }
 
 
