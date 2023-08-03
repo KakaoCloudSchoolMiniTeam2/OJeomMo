250e39bc647067bb772214bab6dfc89570883f63
TaeMin Kim <tk4003950@gmail.com>
2023-08-03 17:54:49 +0900

    ✨	: 댓글 CRUD 서비스 로직 구현
    - Member 엔티티 동일 엔티티인지 검사하는 equals 메서드 재정의

--- src/main/java/org/kcsmini2/ojeommo/member/data/entity/Member.java
+++ src/main/java/org/kcsmini2/ojeommo/member/data/entity/Member.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -64,7 +65,18 @@ public class Member{
         catch (Exception e) {
             return false;
         }
-
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
