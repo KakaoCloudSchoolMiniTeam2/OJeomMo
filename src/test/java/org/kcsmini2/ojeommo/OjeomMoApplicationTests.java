3baf9b565ea0592646eb1c91598554fcc70ac91b
Kim Yeoun Su <74704587+idkim97@users.noreply.github.com>
2023-08-16 01:06:00 +0900

    ✅	: 불필요한 @SpringBootTest 삭제 ( 배포 시 문제 발생 )

--- src/test/java/org/kcsmini2/ojeommo/OjeomMoApplicationTests.java
+++ src/test/java/org/kcsmini2/ojeommo/OjeomMoApplicationTests.java
@@ -3,7 +3,7 @@ package org.kcsmini2.ojeommo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OJeomMoApplicationTests {
 
 	@Test
