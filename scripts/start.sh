9eeab579e6ade3815c2ba4387033bef1c2657b17
Kim Yeoun Su <74704587+idkim97@users.noreply.github.com>
2023-08-16 22:32:49 +0900

    💚	: start.sh cp 위치 바꿔봄

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,8 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+#cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $JAR_FILE $PROJECT_ROOT/build/libs/*.jar
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
