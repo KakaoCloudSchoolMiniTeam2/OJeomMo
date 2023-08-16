09a488646055e42df78c387d6617355fa1acb435
Kim Yeoun Su <74704587+idkim97@users.noreply.github.com>
2023-08-16 22:47:29 +0900

    💚	: start.sh, stop.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/app/step1/OJeomMo" # 루트 경로
-JAR_FILE="/home/ec2-user/app/step2/zip/build/libs/OJeomMo-1.1.1.jar"
+BUILD_DIR="/home/ec2-user/app/step2/zip/build/libs"
+JAR_NAME="OJeomMo-1.1.1.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -10,13 +11,15 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+echo "$TIME_NOW > $JAR_NAME 파일 복사" >> $DEPLOY_LOG
 #cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-cp $JAR_FILE $PROJECT_ROOT/build/libs/*.jar
+#cp $JAR_FILE $PROJECT_ROOT/build/libs/*.jar
+cp "$BUILD_DIR/$JAR_NAME" "$PROJECT_ROOT/build/libs/$JAR_NAME"
 
 # jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
+#nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar "$PROJECT_ROOT/build/libs/$JAR_NAME" > "$APP_LOG" 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/app/step1/OJeomMo"
-JAR_FILE="/home/ec2-user/app/step2/zip/build/libs/OJeomMo-1.1.1.jar" # 여기서 프로젝트 이름은 일단 원하는 프로젝트 이름으로 설정해주세요. 밑에서 추가 설명하겠습니다.
+#JAR_FILE="/home/ec2-user/app/step2/zip/build/libs/OJeomMo-1.1.1.jar" # 여기서 프로젝트 이름은 일단 원하는 프로젝트 이름으로 설정해주세요. 밑에서 추가 설명하겠습니다.
+JAR_NAME="OJeomMo-1.1.1.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f "$PROJECT_ROOT/build/libs/$JAR_NAME")
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> "$DEPLOY_LOG"
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> "$DEPLOY_LOG"
+  kill -15 "$CURRENT_PID"
 fi
