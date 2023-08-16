bd41c482a4144c50c472bea9f582f8ad655d3404
Kim Yeoun Su <74704587+idkim97@users.noreply.github.com>
2023-08-16 23:03:33 +0900

    💚	: 배포된 jar파일로 실행되는지 확인

--- scripts/start.sh
+++ scripts/start.sh
@@ -19,7 +19,7 @@ cp "$BUILD_DIR/$JAR_NAME" "$PROJECT_ROOT/build/libs/$JAR_NAME"
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
 #nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-nohup java -jar "$PROJECT_ROOT/build/libs/$JAR_NAME" > "$APP_LOG" 2> $ERROR_LOG &
+nohup java -jar "$BUILD_DIR/$JAR_NAME" > "$APP_LOG" 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
